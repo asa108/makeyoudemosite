8fc59af8b7988f1d8964cbc9c276d3acf8283ec9
Asane <asane@AsanenoMacBook-Air.local>
2020-08-22 18:55:10 +0900

    path　トップ画像　変更

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <!-- <h2>Home page</h2>
     <button @click="toAbout">about ページにいく</button> -->
     <div class="image_container">
-      <img class="home_img" :src="src" alt="top image" />
+      <img class="home_img" :src="image_src" alt="top image" />
       <div class="title">MAKE YOU SUMMER SALE</div>
     </div>
     <div class="home">
@@ -30,7 +30,7 @@ export default {
     },
     data() {
       return {
-        src: "./img/top.jpg",
+        image_src: require("../assets/top.jpg"),
       };
     },
   },
